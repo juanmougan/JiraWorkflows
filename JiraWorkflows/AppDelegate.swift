eb13ce223e0adfef6dc5785a55aa4c08fd75f49a
Juan Manuel Mougan <juanmougan@gmail.com>
2020-11-06 19:46:16 -0300

    Cleaned up code

--- JiraWorkflows/AppDelegate.swift
+++ JiraWorkflows/AppDelegate.swift
@@ -10,47 +10,26 @@ import SwiftUI
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    var popover: NSPopover!
-    var statusBarItem: NSStatusItem!
-
+    
+    var popover = NSPopover.init()
+    var statusBar: StatusBarController?
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
+        
+        // Create the SwiftUI view that provides the contents
         let contentView = ContentView()
         
-        // Create the popover
-        let popover = NSPopover()
-        popover.contentSize = NSSize(width: 400, height: 400)
-        popover.behavior = .transient
+        // Set the SwiftUI's ContentView to the Popover's ContentViewController
+        popover.contentSize = NSSize(width: 360, height: 360)
         popover.contentViewController = NSHostingController(rootView: contentView)
-        self.popover = popover
-        // Make buttons clickable
-//        self.popover.contentViewController?.view.window?.becomeKey()
-                
-        // Create the status item
-        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
-        if let button = self.statusBarItem.button {
-             button.image = NSImage(named: "Icon")
-             button.action = #selector(togglePopover(_:))
-        }
-        NSApp.activate(ignoringOtherApps: true)
+        
+        // Create the Status Bar Item with the above Popover
+        statusBar = StatusBarController.init(popover)
+        //        NSApp.activate(ignoringOtherApps: true)
     }
     
-    // Create the status item
-    @objc func togglePopover(_ sender: AnyObject?) {
-         if let button = self.statusBarItem.button {
-              if self.popover.isShown {
-                   self.popover.performClose(sender)
-              } else {
-                   self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
-              }
-         }
-    }
-
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
 }
-
