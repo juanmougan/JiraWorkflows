381b58b8b698f44ad986dca1a45a1bc1b146dfc0
Juan Manuel Mougan <juanmougan@gmail.com>
2020-11-09 20:24:41 -0300

    JSON deserialization of the enum fixed

--- JiraWorkflows/Worklog.swift
+++ JiraWorkflows/Worklog.swift
@@ -8,23 +8,33 @@
 import Foundation
 
 
-enum Status: String, Codable {
+/// This is a Worklog possible status
+enum Status: String, Codable, Equatable {
     case below = "below"
     case ok = "ok"
     case overtime = "overtime"
+    
+    public init(from decoder: Decoder) throws {
+        // If decoding fails, we default to "below"
+        guard let rawValue = try? decoder.singleValueContainer().decode(String.self) else {
+            self = .below
+            return
+        }
+        self = Status(rawValue: rawValue) ?? .below
+    }
 }
 
-// This is the expected object
+/// This represents a Worklog on JIRA
 struct Worklog: Codable {
     var minutesLogged: Int
     var totalTickets: Int
     var status: Status
 }
 
-// Parse JSON from the JAR's stdout
+/// Parses a JSON obtained from the JAR's stdout
 func parse(json: String) -> Worklog {
     let decoder = JSONDecoder()
-
+    
     let data = Data(json.utf8)
     if let jsonWorklogs = try? decoder.decode(Worklog.self, from: data) {
         return jsonWorklogs
