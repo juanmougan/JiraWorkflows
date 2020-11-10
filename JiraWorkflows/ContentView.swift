d10d2798fbee3de8390e14f80dd7d16ed5544f43
Juan Manuel Mougan <juanmougan@gmail.com>
2020-11-10 13:08:54 -0300

    Trying to refresh the UI, still not working

--- JiraWorkflows/ContentView.swift
+++ JiraWorkflows/ContentView.swift
@@ -12,10 +12,14 @@ final class JiraData: ObservableObject {
     init() {
         // TODO use real service
         self.worklog = self.jiraService.getCounter(tickets: 2, minutes: 120, status: "BELOW")
+        self.timeAndTicketsMsg = "You logged \(String(format: "%.2f", Double(self.worklog.minutesLogged) / 60.0)) hours in \(self.worklog.totalTickets) tickets today"
+        self.statusMsg = "You are on \"\(self.worklog.status.rawValue)\" status"
     }
     
     init(hours: Double, tickets: Int, status: Status) {
         self.worklog = Worklog(minutesLogged: Int(hours) * 60, totalTickets: tickets, status: status)
+        self.timeAndTicketsMsg = "You logged \(String(format: "%.2f", Double(self.worklog.minutesLogged) / 60.0)) hours in \(self.worklog.totalTickets) tickets today"
+        self.statusMsg = "You are on \"\(self.worklog.status.rawValue)\" status"
     }
     
     /// Refreshes the data in this object, by calling the underlying service again
@@ -24,19 +28,16 @@ final class JiraData: ObservableObject {
     func refresh() {
         self.worklog = self.jiraService.getCounter(tickets: Int.random(in: 3..<5), minutes: 390, status: "OK")
         print("Now mocked is: \(self.worklog)")
-    }
-    
-    func getTimeAndTickets() -> String {
-        return "You logged \(String(format: "%.2f", Double(self.worklog.minutesLogged) / 60.0)) hours in \(self.worklog.totalTickets) tickets today"
-    }
-    
-    func getStatus() -> String {
-        // TODO removed hardcoded part
-        return "You are on \"\(Status.below.rawValue)\" status"
+        self.timeAndTicketsMsg = "You logged \(String(format: "%.2f", Double(self.worklog.minutesLogged) / 60.0)) hours in \(self.worklog.totalTickets) tickets today"
+        print(self.timeAndTicketsMsg)
+        self.statusMsg = "You are on \"\(self.worklog.status.rawValue)\" status"
+        print(statusMsg)
     }
     
     var jiraService = MockedService()
-    @Published var worklog: Worklog
+    var worklog: Worklog
+    @Published var timeAndTicketsMsg: String
+    @Published var statusMsg: String
 }
 
 struct ContentView: View {
@@ -44,13 +45,13 @@ struct ContentView: View {
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-            Text(jiraData.getTimeAndTickets())
+            Text(jiraData.timeAndTicketsMsg)
                 .font(Font.system(size: 20.0))
                 .fontWeight(.semibold)
                 .multilineTextAlignment(.leading)
                 .padding(.horizontal, 16.0)
                 .frame(width: 360.0, height: 80.0, alignment: .topLeading)
-            Text(jiraData.getStatus())
+            Text(jiraData.statusMsg)
                 .font(Font.system(size: 20.0))
                 .fontWeight(.semibold)
                 .multilineTextAlignment(.leading)
